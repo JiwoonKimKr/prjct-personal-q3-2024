4aa8e28bfb464327e14e37d1399950c478ca0b7b
syjnk@naver.com <syjnk@naver.com>
2024-08-01 18:13:01 +0900

    메인 홈페이지(로그인)와 회원가입 마크업_01 08 2024

---
+++ src/main/java/com/givemetreat/user/UserController.java
@@ -0,0 +1,18 @@
+package com.givemetreat.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/")
+	public String signInView() {
+		return "user/sign-in";
+	}
+
+	@GetMapping("/user/sign-up-view")
+	public String signUpView() {
+		return "user/sign-up";
+	}
+}
