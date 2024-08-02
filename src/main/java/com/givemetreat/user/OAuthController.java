e3eba9ac46a940209fe8d83dd839ce0a7896abc3
syjnk@naver.com <syjnk@naver.com>
2024-08-03 04:49:17 +0900

    Controller에 로그인 처리까지 기재_다시 검증해야_03 08 2024

--- src/main/java/com/givemetreat/user/OAuthController.java
+++ src/main/java/com/givemetreat/user/OAuthController.java
@@ -1,6 +1,7 @@
 package com.givemetreat.user;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
@@ -9,16 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.givemetreat.api.dto.ResponseTokenKakaoApi;
 import com.givemetreat.api.dto.UserInfoKakaoApi;
 import com.givemetreat.api.utils.PrivateKeysKakaoApi;
+import com.givemetreat.common.EncryptUtils;
+import com.givemetreat.user.bo.UserBO;
+import com.givemetreat.user.domain.UserEntity;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 public class OAuthController {
+	private final UserBO userBO;
 
+	@SuppressWarnings("unchecked")
 	@GetMapping("/OAuth/kakao-sign-up")
 	public String redirectedFromKakaoSignUp(
 			@RequestParam(required = false) String code
@@ -44,19 +55,56 @@ public class OAuthController {
 		
 		log.info("[OAuth: Kakao Api: 3rd] Response As User Info:{}", userInfo);
 		
-		Integer id = Integer.parseInt(userInfo.getId());
+		String idUserInfo = userInfo.getId();
 		String email = (String) userInfo.getKakao_account().get("email");
-		@SuppressWarnings("unchecked")
 		Map<String, Object> profile = (Map<String, Object>) userInfo.getKakao_account().get("profile");
 		String nickname  = (String) profile.get("nickname");
 		
-		if(id == null || email == null || nickname == null) {
+		if(idUserInfo == null || email == null || nickname == null) {
 			return "redirect: localhost/";
 		}
 		
+		//이미 이메일이 DB에 존재하는 경우, 비밀번호 일치하는지 확인해야
+		UserEntity userCur = userBO.getUserByLoginId(email);
+		if(userCur != null) {
+			MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+			formData.add("loginId", email);
+			formData.add("password", idUserInfo);
+			WebClient webClient = WebClient.builder().build();
+			String response = webClient.post()
+							.bodyValue(formData)
+							.retrieve()
+							.bodyToMono(String.class)
+							.block();
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			Map<String, Object> mapResponse = new HashMap<>();
+			try {
+				mapResponse = objectMapper.readValue(response, Map.class);
+			} catch (JsonMappingException e) {
+				return "redirect: localhost/";
+			} catch (JsonProcessingException e) {
+				return "redirect: localhost/";
+			}
+			
+			if((Integer) mapResponse.get("code") == 200) {
+				return "redirect: /product/product-list-view";
+			} else {
+				return "redirect: localhost/";
+			}
+			
+		}
 		
+		String salt = EncryptUtils.getSalt();
+		String password = EncryptUtils.sha256(salt, idUserInfo);
 		
-		return "redirect: /product/productList";
+		UserEntity user = userBO.addUser(email, password, salt, nickname);
+		
+		if(user == null) {
+			return "redirect: localhost/";
+		}
+		
+		return "redirect: localhost/product/productList";
 	}
 	
 	private ResponseTokenKakaoApi postRequestWithCodeForToken(String code){
