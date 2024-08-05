f4ba89f31816d7b1e0f1b538cf33d6c6c806492f
syjnk@naver.com <syjnk@naver.com>
2024-08-05 13:33:46 +0900

    ajax를 통해 Post Request로 받아온 JSON 값을 HTML에 뿌리는 작업을 처음 실행함_하지만 LocalDateTime이 넘어오지 않아서 고민_05 08 2024

--- src/main/java/com/givemetreat/product/AdminProductController.java
+++ src/main/java/com/givemetreat/product/AdminProductController.java
@@ -4,12 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.givemetreat.product.bo.ProductBO;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RequestMapping("/admin/product")
 @Controller
 public class AdminProductController {
-
+	private final ProductBO productBO;
+	
 	@GetMapping("/product-register-view")
 	public String productRegisterView() {
 		return "admin/product/productRegister";
 	}
+	//상품 상세 조회
+	@GetMapping("/product-detail-view")
+	public String productDetailView(){
+		return "admin/product/productDetail";
+	}	
 }
--- src/main/java/com/givemetreat/product/AdminProductRestController.java
+++ src/main/java/com/givemetreat/product/AdminProductRestController.java
@@ -1,8 +1,9 @@
 package com.givemetreat.product;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,30 +13,42 @@ import com.givemetreat.product.bo.ProductBO;
 import com.givemetreat.product.domain.Product;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequestMapping("/admin/product")
 @RequiredArgsConstructor
 @RestController
 public class AdminProductRestController {
 	private final ProductBO productBO;
 	
-	//상품 상세 조회
-	@PostMapping("/product-detail")
-	public Map<String, Object> productDetail(
+	@PostMapping("/product-detail-view")
+	public Map<String, Object> productDetailView(
 				@RequestParam(required = false) Integer id 
 				, @RequestParam(required = false) String name 
 				, @RequestParam(required = false) String category
 				, @RequestParam(required = false) Integer price
 				, @RequestParam(required = false) String agePetProper
-				, Model model){
+				){
 		Map<String, Object> result = new HashMap<>();
+		log.info("[ADMIN-Product: RequestParam for Searching product detail:{},{},{},{},{}]", id, name, category, price, agePetProper);
+		
+		if(name != null && name.isBlank()) {
+			name = null;
+		}
+		if(category != null && category.isBlank()) {
+			category = null;
+		}
+		if(agePetProper != null && agePetProper.isBlank()) {
+			agePetProper = null;
+		}
 		
 		List<Product> listProducts = productBO.getProduct(id, name, category, price, agePetProper);
 		
-		model.addAttribute(listProducts);
-		
 		result.put("code", 200);
 		result.put("result", "success");
+		result.put("listProducts", listProducts);
+		
 		return result;
 	}
 }
