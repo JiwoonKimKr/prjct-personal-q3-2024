b295fed5dbb98337e23f5b86ea2321f5c3ae4056
syjnk@naver.com <syjnk@naver.com>
2024-08-06 17:59:17 +0900

    최근결제 내역 상세 조회 관련 마크업&API 마무리_06 08 2024

--- src/test/java/com/givemetreat/GivemetreatApplicationTests.java
+++ src/test/java/com/givemetreat/GivemetreatApplicationTests.java
@@ -1,13 +1,18 @@
 package com.givemetreat;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //이건 스프링 서버를 모두 불러오는 어노테이션
 class GivemetreatApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void 더하기테스트() { //한글로 명칭을 작성 가능! 테스트 전용 메소드라는 걸 바로 알 수 있다!
+		int x = 15;
+		int y = 20;
+		assertEquals(x, y, 35); //뒤에 정답을 적어야 확인 가능하다! 
 	}
 
 }
---
+++ src/test/java/com/givemetreat/user/bo/UserBOTest.java
@@ -0,0 +1,34 @@
+package com.givemetreat.user.bo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.givemetreat.common.EncryptUtils;
+import com.givemetreat.user.domain.UserEntity;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class UserBOTest {
+	@Autowired
+	UserBO userBO;
+	
+	@Test
+	void 회원가입비밀번호체크() {
+		String loginId = "qwerty@qwerty.com";
+		String password = "aaaa";
+		String nickname = "천상의도적_치지직";
+				
+		String salt = "fff574b245d4a0afc07153687138c219730c024e";
+		String hashedPassword = EncryptUtils.sha256(salt, password);
+		UserEntity user = userBO.addUser(loginId, hashedPassword, salt, nickname);
+		log.info("[userBO_Test: 회원가입 관련 비밀번호 Salt + SHA256 방식 검증] hashedPassword:{}, salt:{}", hashedPassword, salt);
+		assertEquals(user.getPassword(), "7cc68c9637d50b3676c36c45bcf1ea6549bd3176b2e7bcf703786c8c7bbfb080");
+	}
+	
+	
+}
