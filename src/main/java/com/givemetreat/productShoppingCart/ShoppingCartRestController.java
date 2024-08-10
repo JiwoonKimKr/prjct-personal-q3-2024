b5cd62b67a43ffd9f731220648883c47884f8613
syjnk@naver.com <syjnk@naver.com>
2024-08-10 22:34:13 +0900

    장바구니 해당 항목 '취소하기' 버튼 삭제API_AJAX Post로 구현_10 08 2024

--- src/main/java/com/givemetreat/productShoppingCart/ShoppingCartRestController.java
+++ src/main/java/com/givemetreat/productShoppingCart/ShoppingCartRestController.java
@@ -71,7 +71,7 @@ public class ShoppingCartRestController {
 		Integer userId = (Integer) session.getAttribute("userId");
 		
 		if(userId == null) {
-		log.info("[UserShoppingCartRestController addProduct()] userId is null now. Sign-In needed.");
+		log.info("[UserShoppingCartRestController substractProduct()] userId is null now. Sign-In needed.");
 		result.put("code", 403);
 		result.put("error_message", "로그인 후 이용 가능합니다.");
 		return result;			
@@ -83,17 +83,51 @@ public class ShoppingCartRestController {
 										, cartItemId);
 		
 		if(itemEnlisted == null) {
-		log.info("[UserShoppingCartRestController addProduct()] current item got failed to get enlisted in cart.");
+		log.info("[UserShoppingCartRestController substractProduct()] current item got failed to get enlisted in cart.");
 		result.put("code", 500);
 		result.put("error_message", "해당 상품과 수량을 장바구니에서 수정하지 못하였습니다..");
 		return result;
 		}
 		
-		log.info("[UserShoppingCartRestController addProduct()] success to enlist current item in cart.");
+		log.info("[UserShoppingCartRestController substractProduct()] success to enlist current item in cart.");
 		result.put("code", 200);
 		result.put("success", "해당 상품 및 수량을 장바구니에서 수정하였습니다.");
 		result.put("quantity", itemEnlisted.getQuantity());
 		
 		return result;
+	}
+	
+	@PostMapping("/delete-product")
+	public Map<String, Object> deleteProduct(@RequestParam int productId
+												, @RequestParam Integer cartItemId
+												, HttpSession session){
+		Map<String, Object> result = new HashMap<>();
+		
+		Integer userId = (Integer) session.getAttribute("userId");
+		if(userId == null) {
+		log.info("[UserShoppingCartRestController deleteProduct()] userId is null now. Sign-In needed.");
+		result.put("code", 403);
+		result.put("error_message", "로그인 후 이용 가능합니다.");
+		return result;			
+		}
+		
+		ProductShoppingCartEntity itemEnlisted = productShoppingCartBO.updateQuantity(userId
+										, productId
+										, 0
+										, cartItemId);
+		
+		if(itemEnlisted == null || itemEnlisted.getQuantity() != 0) {
+		log.info("[UserShoppingCartRestController deleteProduct()] current item got failed to get deleted in cart.");
+		result.put("code", 500);
+		result.put("error_message", "해당 상품과 수량을 장바구니에서 삭제하지 못하였습니다..");
+		return result;
+		}
+		
+		log.info("[UserShoppingCartRestController deleteProduct()] success to delete current item in cart.");
+		result.put("code", 200);
+		result.put("success", "해당 상품 및 수량을 장바구니에서 삭제하였습니다.");
+		result.put("quantity", itemEnlisted.getQuantity());
+		
+		return result;		
 	}
 }
